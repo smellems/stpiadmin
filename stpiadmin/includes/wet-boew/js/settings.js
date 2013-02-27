0a6751fac6b39af93283708a39f8cb6603a5f4d3
smellems <smellems@gmail.com>
2013-02-26 20:00:51 -0500

    Mise à jour WET v3.0.5.

--- stpiadmin/includes/wet-boew/js/settings.js
+++ stpiadmin/includes/wet-boew/js/settings.js
@@ -3,7 +3,7 @@
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * wet-boew.github.com/wet-boew/License-eng.txt / wet-boew.github.com/wet-boew/Licence-fra.txt
  *
- * Version: v3.1.0-b1 Build: 2012-12-21 05:01 PM
+ * Version: v3.0.5 Build: 2013-02-15 10:10 PM
  *
  */
 /*
@@ -11,7 +11,7 @@ WET-BOEW-Settings
  */
 var wet_boew_properties = {
 	/** global plugins are called via a array of dependency names **/
-	globals : ['equalize', 'deselectradio', 'css3ie', 'datemodified']
+	globals : ['equalize', 'deselectradio', 'css3ie']
 };
 
 /*

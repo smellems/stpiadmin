0d59b470b23d7c3169e0f4acdbbdaa9b9dc512e1
smellems <smellems@gmail.com>
2013-12-17 16:29:39 -0500

    update WET-BOEW 3.1.4

--- stpiadmin/includes/wet-boew/js/settings.js
+++ stpiadmin/includes/wet-boew/js/settings.js
@@ -1,17 +1,19 @@
 /*!
  *
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
- * wet-boew.github.io/wet-boew/License-eng.txt / wet-boew.github.io/wet-boew/Licence-fra.txt
+ * wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  *
- * Version: v3.1.1-release Build: 2013-05-16 04:34 AM
+ * Version: v3.1.4-release Build: 2013-09-17 12:23 PM EDT
  *
  */
 /*
 WET-BOEW-Settings
  */
+
+/* exported wet_boew_properties, wet_boew_share */
 var wet_boew_properties = {
-	/** global plugins are called via an array of dependency names **/
-	globals : ['deselectradio', 'datemodified']
+	/** global plugins are called via a array of dependency names **/
+	"globals" : ["deselectradio", "datemodified"]
 };
 
 /*
@@ -20,5 +22,5 @@ Global overrides for individual components
 
 // Share widget
 var wet_boew_share = {
-	sites : ['del.icio.us', 'digg', 'facebook', 'google', 'linkedin', 'reddit', 'stumbleupon', 'twitter', 'yahoobuzz']
+	"sites" : ["del.icio.us", "digg", "facebook", "google", "linkedin", "reddit", "stumbleupon", "twitter", "yahoobuzz"]
 };
